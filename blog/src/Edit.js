0171319d53107559b1171afb25a3a1a516c6b75b
Madalik <73160318+Madalik@users.noreply.github.com>
2022-01-10 17:14:23 +0200

    Update Edit.js

--- blog/src/Edit.js
+++ blog/src/Edit.js
@@ -1,50 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Edit = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [data, setData] = useState();
+  const blog = { title, body, author };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const blog = { title, body, author };
-    console.log(blog);
+  useEffect(() => {
+    handleSubmit();
+  }, []);
 
+  const handleSubmit = async () => {
     fetch("http://localhost:8000/blogs" + blog.id, {
       method: "PUT",
       headers: { "Content-Type": " application/json" },
       body: JSON.stringify(blog),
-    }).then(() => {
+    }).then((response) => {
       console.log("edit blog");
-     
+      setData(response.data);
+      console.log("data", data);
     });
-
-    return (
-      <div className="create">
-        <h2>Edit Blog</h2>
-        <form onSubmit={handleSubmit}>
-          <label>Blog title:</label>
-          <input
-            type="text"
-            required
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <label>Blog body:</label>
-          <textarea
-            required
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          ></textarea>
-          <label>Blog author:</label>
-          <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-            <option value="mario">mario</option>
-            <option value="yoshi">yoshi</option>
-          </select>
-        </form>
-      </div>
-    );
   };
+
+  return (
+    <div className="create">
+      <h2>Edit Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default Edit;
